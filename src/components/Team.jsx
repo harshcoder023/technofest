d0a61197c31d54979888f0c009e1c71bc1152de4
Harsh <bhardwajharsh023@gmail.com>
2024-02-15 19:57:24 +0530

    hm

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,65 +1,75 @@
 import React from "react";
-import Shreysh from "../assets/shreysh.jpeg";
+import Shreysh from "../assets/shreysh.jpg";
+import Nimit from "../assets/nimit.jpg";
+import Siddharth from "../assets/siddharth.png";
+import Alok from "../assets/alok.jpg";
+import Harsh from "../assets/harsh.jpg";
+import Sahil from "../assets/sahil.jpg";
+import Yash from "../assets/yash.jpg";
+import Tushar from "../assets/tushar.jpg";
 const Team = () => {
   const teamMembers = [
     {
       name: "Tushar Bhardwaj",
-      image:
-        "https://media.gq-magazine.co.uk/photos/6362687ed85a7b93f28a0904/3:2/w_1011,h_674,c_limit/Sigma-grindset-patrick-bateman.jpeg",
+      image: Tushar,
       social: "https://www.instagram.com/tushartom7/",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Siddharth Tayal",
-      image:
-        "https://ibighit.com/bts/images/profile/proof/member/member-jk.jpg",
+      image: Siddharth,
       social:
         "https://www.linkedin.com/in/siddharth-tayal-65203a266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Harsh Goyal",
-      image:
-        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.9092553.1705229888&semt=sph",
+      image: Harsh,
       social:
         "https://www.linkedin.com/in/harsh-goyal-2b6062266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Anmol",
       image:
         "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.9092553.1705229888&semt=sph",
       social: "https://www.instagram.com/anmol_._ahuja/",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Nimit Shekhawat",
-      image:
-        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.9092553.1705229888&semt=sph",
+      image: Nimit,
       social:
         "https://www.linkedin.com/in/nimit-shekhawat?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Maanik Sharma",
       image: "https://wallpapercave.com/wp/wp12140288.jpg",
       social:
         "https://www.linkedin.com/in/maanik-sharma-914795266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Sahil",
-      image:
-        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.9092553.1705229888&semt=sph",
+      image: Sahil,
       social:
         "https://www.linkedin.com/in/sahil-dhillon-294a05208?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 2nd Year",
     },
     {
       name: "Shreysh",
       image: Shreysh,
       social:
         "https://www.linkedin.com/in/shreyashgupta14?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 3rd Year",
     },
     {
       name: "Yash",
-      image:
-        "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.9092553.1705229888&semt=sph",
+      image: Yash,
       social:
         "https://www.linkedin.com/in/yashmit-narang-87a68626a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      class: "B.Tech CSE 2nd Year",
     },
   ];
   return (
@@ -84,18 +94,21 @@ const Team = () => {
             <div className="flex  items-center lg:gap-8 gap-6 max-[410px]:gap-10 border-b-[1px] border-gray-300 pb-6 px-4">
               <div className="image lg:w-32  w-28 lg:h-32  h-28 rounded-full overflow-hidden ">
                 <img
-                  src="https://www.wilsoncenter.org/sites/default/files/media/images/person/james-person-1.jpg"
+                  src={Alok}
                   alt=""
                   className="w-full h-full object-cover object-center"
                 />
               </div>
 
               <div>
-                <h3 className="font-mont text-2xl max-[410px]:text-xl  font-medium">
+                <span className="font-sans inline-block text-xs px-2 py-1 text-white font-medium rounded-lg bg-[#FA7F5C]">
+                  Lead Mentor
+                </span>
+                <h3 className="font-mont text-2xl max-[410px]:text-xl  font-medium mt-1">
                   Mr. Alok Gupta
                 </h3>
                 <span className="font-sans inline-block text-md font-medium">
-                  Lead Mentor
+                  AP, CSE
                 </span>
               </div>
             </div>
@@ -105,7 +118,7 @@ const Team = () => {
         <div className="members lg:w-[85%] lg:mx-auto  flex flex-wrap gap-y-12 gap-x-24 max-[410px]:gap-x-20 lg:gap-16 justify-center pt-6">
           {teamMembers.map((member, index) => (
             <a key={index} href={member.social}>
-              <div className="member lg:w-32  w-28  flex flex-col items-center gap-4">
+              <div className="member lg:w-32  w-28  flex flex-col items-center ">
                 <div
                   className={`image w-full lg:h-32  h-28 rounded-full ${
                     index == 0 ? "border-[#FA7F5C] border-[2px]" : ""
@@ -114,13 +127,20 @@ const Team = () => {
                   <img
                     src={member.image}
                     alt=""
-                    className="w-full h-full object-cover object-center"
+                    className={`w-full h-full object-cover  ${
+                      index == 1 || index == 7 || index == 0
+                        ? "object-top"
+                        : "object-center"
+                    }`}
                   />
                 </div>
 
-                <h3 className="font-mont text-md font-semibold text-center leading-5">
+                <h3 className="font-mont text-md font-semibold text-center leading-5 mt-4 mb-2">
                   {member.name}
                 </h3>
+                <h4 className="text-xs text-black text-center font-medium">
+                  {member.class}
+                </h4>
               </div>
             </a>
           ))}
