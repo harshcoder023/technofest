97769a7a8a408131c0b0b1b2ace5ba7435610fa9
Harsh <bhardwajharsh023@gmail.com>
2024-02-23 18:51:56 +0530

    added 2 featured events

--- src/components/Esport.jsx
+++ src/components/Esport.jsx
@@ -54,6 +54,42 @@ const Esport = () => {
                 </div>
               </div>
             </div>
+            <div className="lg:h-[400px] h-[450px] relative">
+              <img
+                src="https://static.vecteezy.com/system/resources/previews/029/290/604/non_2x/ai-generative-dj-playing-and-mixing-music-in-nightclub-party-at-night-edm-dance-music-club-with-crowd-of-young-people-free-photo.jpg"
+                alt=""
+                className="absolute w-full h-full left-0 top-0 object-cover object-bottom"
+              />
+              <div className="text absolute z-10 w-full h-full left-0 top-0 flex justify-center items-center flex-col">
+                <h3 className="font-mont font-extrabold uppercase text-white  text-5xl drop-shadow-lg shadow-black">
+                  DJ NIGHT
+                </h3>
+                <div className=" text-white flex flex-col items-center">
+                  <p className="mb-8 mt-1 drop-shadow-2xl w-[80%] lg:w-full  shadow-black font-semibold lg:font-medium">
+                    {" "}
+                    Get Your Squad Ready: DJ Night is the Place to Be!
+                  </p>
+                  {/* <a
+                    href="https://forms.gle/Fj9w3KECyPNczVRT7"
+                    className="text-white bg-gray-900  font-medium text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                  >
+                    Join Mission
+                  </a> */}
+                </div>
+              </div>
+            </div>
+            <div className="lg:h-[400px] h-[450px] relative">
+              <img
+                src="https://www.koimoi.com/wp-content/new-galleries/2023/02/ddlj-box-office-re-release-shah-rukh-khan-kajols-magic-shows-a-300-jump-9977-days-later-this-classic-is-still-earning-in-double-digits-lakhs-0001.jpg"
+                alt=""
+                className="absolute w-full h-full left-0 top-0 object-cover object-bottom"
+              />
+              <div className="text absolute z-10 w-full h-full left-0 top-0 flex justify-center items-center flex-col">
+                <h3 className="font-mont font-extrabold uppercase text-white  lg:text-3xl drop-shadow-lg shadow-black lg:leading-7 text-[22px] leading-6">
+                  Bollywood Extravaganza: <br /> Lights, Camera, Action! 🎬
+                </h3>
+              </div>
+            </div>
           </Carousel>
         </div>
       </div>
