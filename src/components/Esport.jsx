e038e95d2700176602bebe7114ce84708bc44a40
Harsh <bhardwajharsh023@gmail.com>
2024-02-25 16:47:26 +0530

    added blind date

--- src/components/Esport.jsx
+++ src/components/Esport.jsx
@@ -20,7 +20,7 @@ const Esport = () => {
           <Carousel
             autoPlay
             emulateTouch
-            infiniteLoop
+            // infiniteLoop
             stopOnHover
             showArrows
             showIndicators
@@ -70,12 +70,6 @@ const Esport = () => {
                     {" "}
                     Get Your Squad Ready: DJ Night is the Place to Be!
                   </p>
-                  {/* <a
-                    href="https://forms.gle/Fj9w3KECyPNczVRT7"
-                    className="text-white bg-gray-900  font-medium text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                  >
-                    Join Mission
-                  </a> */}
                 </div>
               </div>
             </div>
@@ -96,6 +90,24 @@ const Esport = () => {
                 >
                   Know more
                 </Link>
+              </div>
+            </div>
+            <div className="lg:h-[400px] h-[450px] relative">
+              <img
+                src="https://www.india.com/wp-content/uploads/2017/09/blind-dates.jpg"
+                alt=""
+                className="absolute w-full h-full left-0 top-0 object-cover object-center"
+              />
+              <div className="text absolute z-10 w-full h-full left-0 top-0 flex justify-center items-center flex-col">
+                <h3 className="font-mont font-extrabold uppercase text-black  text-5xl drop-shadow-lg shadow-blue-500">
+                  BLIND DATE
+                </h3>
+                <div className=" text-black flex flex-col items-center">
+                  <p className="mb-8 mt-1 drop-shadow-2xl   lg:w-full  shadow-black font-semibold lg:font-medium">
+                    {" "}
+                    Take a Chance, Embrace the Mystery
+                  </p>
+                </div>
               </div>
             </div>
           </Carousel>
